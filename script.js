40bab9115fe4c51de2eb81b57cb91ab2017fd7e5
Leggo414 <johnfernandez2201@gmail.com>
2016-11-26 22:42:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ function setGif() {
 var x = Math.floor((Math.random() * 10) + 1);
 
 if (x === 1) {
-     document.getElementById("upperBlock").style.backgroundImage = "url(beaker.gif)";
+     document.getElementById("upperBlock").style.backgroundImage = "url(gifs/beaker.gif)";
 } else if (x === 2) {
-	document.getElementById("upperBlock").style.backgroundImage = "url(trollFace.gif)";
+	document.getElementById("upperBlock").style.backgroundImage = "url(gifs/trollFace.gif)";
 } else if (x === 3) {
-	document.getElementById("upperBlock").style.backgroundImage = "url(spongebob.gif)";
+	document.getElementById("upperBlock").style.backgroundImage = "url(gifs/spongebob.gif)";
 } else if (x === 4) {
-	document.getElementById("upperBlock").style.backgroundImage = "url(nyancat.gif)";
+	document.getElementById("upperBlock").style.backgroundImage = "url(gifs/nyancat.gif)";
 } else if (x === 5) {
-	document.getElementById("upperBlock").style.backgroundImage = "url(dancememe.gif)";
+	document.getElementById("upperBlock").style.backgroundImage = "url(gifs/dancememe.gif)";
 } else if (x === 6) {
-	document.getElementById("upperBlock").style.backgroundImage = "url(idk.gif)";
+	document.getElementById("upperBlock").style.backgroundImage = "url(gifs/idk.gif)";
 } else if (x === 7) {
-	document.getElementById("upperBlock").style.backgroundImage = "url(teeth.gif)";
+	document.getElementById("upperBlock").style.backgroundImage = "url(gifs/teeth.gif)";
 } else if (x === 8) {
-	document.getElementById("upperBlock").style.backgroundImage = "url(lol.gif)";
+	document.getElementById("upperBlock").style.backgroundImage = "url(gifs/lol.gif)";
 } else if (x === 9) {
-	document.getElementById("upperBlock").style.backgroundImage = "url(happy.gif)";
+	document.getElementById("upperBlock").style.backgroundImage = "url(gifs/happy.gif)";
 } else {
-	document.getElementById("upperBlock").style.backgroundImage = "url(dancing.gif)";
+	document.getElementById("upperBlock").style.backgroundImage = "url(gifs/dancing.gif)";
 }
 }
 
